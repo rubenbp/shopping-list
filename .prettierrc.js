7e904bcf4c12998ac6f8524ae9a9cde4565059f0
Rubén Bernárdez <ruben.bernardez@biko2.com>
2020-04-10 18:30:15 +0200

    añade eslint y prettier

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  semi: false, // no añadir ; al final de las sentencias
+  singleQuote: true, // cadenas entre ' y no ", excepto si hay que escapar
+  trailingComma: 'all', // añadir siempre , al final en multilíneas
+  jsxBracketSameLine: false, // cierre con > en línea propia en multilínea
+}
