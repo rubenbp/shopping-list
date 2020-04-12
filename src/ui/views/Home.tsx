4ace127b744b555e92e87f00896e7f62a13f6b48
Rubén Bernárdez <ruben.bernardez@biko2.com>
2020-04-12 11:43:48 +0200

    extract login view

--- src/ui/views/Home.tsx
+++ src/ui/views/Home.tsx
@@ -1,11 +1,7 @@
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import HowToRegIcon from '@material-ui/icons/HowToReg'
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import styled from 'styled-components/macro'
-import { login } from '../../core/actions/auth'
 import { useSession } from '../hooks/useSession'
+import { Login } from './Login'
 import { ProductList } from './ProductList'
 import { AppBar } from './_components/AppBar'
 import { LoadingView } from './_components/LoadingView'
@@ -18,7 +14,7 @@ export const Home = () => {
   }
 
   if (!user) {
-    return <LoginView />
+    return <Login />
   }
 
   return (
@@ -31,36 +27,3 @@ export const Home = () => {
     </AppBar>
   )
 }
-
-const LoginView = () => (
-  <Center>
-    <Typography variant="h3" gutterBottom align="center">
-      ¡Hola!
-    </Typography>
-    <Button
-      variant="contained"
-      color="primary"
-      size="large"
-      startIcon={<HowToRegIcon />}
-      onClick={() => login()}
-    >
-      Identificarme
-    </Button>
-  </Center>
-)
-
-const Center: React.FC = ({ children }) => (
-  <CenterWrapper>
-    <div>{children}</div>
-  </CenterWrapper>
-)
-
-const CenterWrapper = styled.div`
-  position: fixed;
-  display: flex;
-
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-`
---
+++ src/ui/views/Login.tsx
@@ -0,0 +1,39 @@
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import HowToRegIcon from '@material-ui/icons/HowToReg'
+import React from 'react'
+import styled from 'styled-components/macro'
+import { login } from '../../core/actions/auth'
+
+export const Login = () => (
+  <Center>
+    <Typography variant="h3" gutterBottom align="center">
+      ¡Hola!
+    </Typography>
+    <Button
+      variant="contained"
+      color="primary"
+      size="large"
+      startIcon={<HowToRegIcon />}
+      onClick={() => login()}
+    >
+      Identificarme
+    </Button>
+  </Center>
+)
+
+const Center: React.FC = ({ children }) => (
+  <CenterWrapper>
+    <div>{children}</div>
+  </CenterWrapper>
+)
+
+const CenterWrapper = styled.div`
+  position: fixed;
+  display: flex;
+
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`
